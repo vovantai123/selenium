d0c17a08ed0fd47002ba87d48c79f485241842c7
vovantai123 <votai596@gmail.com>
2025-06-11 13:32:55 +0700

    Update baitapseleniumtuan4.py

--- baitapseleniumtuan4.py
+++ baitapseleniumtuan4.py
@@ -2,35 +2,45 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
+import time
 chromedriver_path = "chromedriver.exe"
 service = ChromeService(executable_path=chromedriver_path)
 driver = webdriver.Chrome(service=service)
-driver.get(" https://www.saucedemo.com")
-driver.implicitly_wait(10)
-driver.find_element(By.NAME, "user-name").send_keys("standard_user")
-driver.implicitly_wait(10)
-driver.find_element(By.NAME, "password").send_keys("secret_sauce")
-driver.implicitly_wait(10)
-driver.find_element(By.NAME, "login-button").click()
-products = driver.find_elements(By.CLASS_NAME, "inventory_item")
-data = []
 
-for product in products:
-    driver.implicitly_wait(30)
-    name = product.find_element(By.CLASS_NAME, "inventory_item_name").text
-    price = product.find_element(By.CLASS_NAME, "inventory_item_price").text
-    data.append({"Product Name": name, "Price": price})
+tai = ["standard_user","locked_out_user","problem_user","performance_glitch_user","error_user","visual_user"]
+for tai11 in tai:
+    driver.get(" https://www.saucedemo.com")
+    driver.implicitly_wait(10)
+    driver.find_element(By.NAME, "user-name").send_keys(str(tai11))
+    driver.implicitly_wait(10)
+    driver.find_element(By.NAME, "password").send_keys("secret_sauce")
+    driver.implicitly_wait(10)
+    driver.find_element(By.NAME, "login-button").click()
+    time.sleep(1)
+    if "Epic sadface: Sorry, this user has been locked out."    in driver.page_source:
+        pass
+    else:
+        products = driver.find_elements(By.CLASS_NAME, "inventory_item")
+        data = []
 
+        for product in products:
+            driver.implicitly_wait(30)
+            name = product.find_element(By.CLASS_NAME, "inventory_item_name").text
+            price = product.find_element(By.CLASS_NAME, "inventory_item_price").text
+            data.append({"Product Name": name, "Price": price})
 
-df = pd.DataFrame(data)
-df.to_excel("products.xlsx", index=False)
+        # Lưu vào Excel
+        df = pd.DataFrame(data)
+        df.to_excel("products.xlsx", index=False)
 
-print("Đã lưu danh sách sản phẩm vào 'products.xlsx'.")
+        print("✅ Đã lưu danh sách sản phẩm vào 'products.xlsx'.")
+
 
 
 driver.get("https://thuvienphapluat.vn/ma-so-thue/tra-cuu-ma-so-thue-doanh-nghiep")
 driver.implicitly_wait(10)
 
+# Lấy dữ liệu từ class
 elements = driver.find_elements(By.CLASS_NAME, "item_mst") + driver.find_elements(By.CLASS_NAME, "item_mst_o")
 
 data = []
